4fd5d2f1c596819c1e69bda987e881b3881d1026
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-18 18:12:16 +0200

    Prepare to the next challenge.

---
+++ descendingOrder/src/test/java/DescendingOrderSolutionTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DescendingOrderSolutionTest {
+
+    @Test
+    public void test_01() {
+        assertEquals(0, DescendingOrderSolution.sortDesc(0));
+    }
+
+    @Test
+    public void test_02() {
+        assertEquals(51, DescendingOrderSolution.sortDesc(15));
+    }
+
+
+    @Test
+    public void test_03() {
+        assertEquals(987654321, DescendingOrderSolution.sortDesc(123456789));
+    }
+
+}
