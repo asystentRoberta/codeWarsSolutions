a9b16bb2af322170960cff022a4b43209b7f20cb
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-18 10:33:15 +0200

    Solution of Dubstep challenge.

--- Dubstep/src/main/java/Dubstep.java
+++ Dubstep/src/main/java/Dubstep.java
@@ -1,7 +1,10 @@
 public class Dubstep {
 
-    public static String songDecoder(String song){
+    public static String songDecoder(String song) {
+
+        song = song.replace("WUB", " ");
+        song = song.trim().replaceAll("(\\s)+", " ");
+
         return song;
     }
-
 }
