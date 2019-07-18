ce2c4a635bd134ca005b8c78b0ec133f711ad939
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-18 19:41:02 +0200

    Some clever solutions.

--- descendingOrder/src/main/java/DescendingOrderSolution.java
+++ descendingOrder/src/main/java/DescendingOrderSolution.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,5 +27,32 @@ public class DescendingOrderSolution {
                 listOfDigits.stream()
                         .map(String::valueOf)
                         .collect(Collectors.joining()));
+    }
+
+    public static int sortDescWithStream(final int num) {
+
+        return Integer.parseInt(
+                String.valueOf(num)
+                        .chars()
+                        .mapToObj(i -> String.valueOf(Character.getNumericValue(i)))
+                        .sorted(Comparator.reverseOrder())
+                        .collect(Collectors.joining()));
+    }
+
+    public static int sortDescShortOne(final int num) {
+
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.valueOf(String.join("", array));
+    }
+
+    public static int sortDescWithStreamAnotherOne(final int num) {
+
+        return Integer.parseInt(
+                Integer.toString(num).codePoints()
+                        .sorted()
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .reverse()
+                        .toString());
     }
 }
--- descendingOrder/src/test/java/DescendingOrderSolutionTest.java
+++ descendingOrder/src/test/java/DescendingOrderSolutionTest.java
@@ -1,28 +1,30 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class DescendingOrderSolutionTest {
 
     @Test
     public void test_01() {
-        assertEquals(0, DescendingOrderSolution.sortDesc(0));
+
+        assertEquals(987654321, DescendingOrderSolution.sortDescShortOne(123456789));
     }
 
     @Test
     public void test_02() {
-        assertEquals(51, DescendingOrderSolution.sortDesc(15));
-    }
 
+        assertEquals(987654321, DescendingOrderSolution.sortDescWithStream(123456789));
+    }
 
     @Test
     public void test_03() {
-        assertEquals(987654321, DescendingOrderSolution.sortDesc(123456789));
+
+        assertEquals(987654321, DescendingOrderSolution.sortDescWithStreamAnotherOne(123456789));
     }
 
     @Test
-    public void test_04_fromWeb(){
-        assertEquals(54421, DescendingOrderSolution.sortDesc(21445));
-    }
+    public void test_04_fromWeb() {
 
+        assertEquals(987654321, DescendingOrderSolution.sortDesc(123456789));
+    }
 }
