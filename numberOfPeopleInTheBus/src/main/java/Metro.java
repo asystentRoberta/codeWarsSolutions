c2e6161adafbd0f12240e1dff5727cf9bd0e49eb
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-19 17:35:58 +0200

    Solved number of people in the bus

--- numberOfPeopleInTheBus/src/main/java/Metro.java
+++ numberOfPeopleInTheBus/src/main/java/Metro.java
@@ -2,8 +2,21 @@ import java.util.ArrayList;
 
 public class Metro {
 
-    public static int countPassengers(ArrayList<int[]> stops){
-        return 0;
-    }
+    public static int countPassengers(ArrayList<int[]> stops) {
 
+        int sumOfPassenger = 0;
+
+        for (int[] stop : stops) {
+            int exitParameter = 0;
+            for (int i : stop) {
+                if (exitParameter == 1) {
+                    i *= -1;
+                }
+                sumOfPassenger += i;
+                exitParameter = 1;
+            }
+        }
+
+        return sumOfPassenger;
+    }
 }
