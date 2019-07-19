262501f09dda5b60d97a7b0a1397df7592086af1
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-19 23:17:19 +0200

    Printer errors - solved.

--- printerErrors/src/main/java/Printer.java
+++ printerErrors/src/main/java/Printer.java
@@ -1,7 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Printer {
 
-    public static String printerError(String s){
-        return "toSolve";
-    }
+    public static String printerError(String s) {
 
+        int lengthOfString = s.length();
+
+        String regex = "[n-z]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+
+        int counterOfErrors = 0;
+        while (matcher.find()) {
+            counterOfErrors++;
+        }
+
+        return counterOfErrors
+                + "/"
+                + lengthOfString;
+    }
 }
