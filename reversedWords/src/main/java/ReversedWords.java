da633bb30a905935ee872a7b394edeb97ce8d0cf
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-21 00:11:52 +0200

    Reversed Words Solved.

--- reversedWords/src/main/java/ReversedWords.java
+++ reversedWords/src/main/java/ReversedWords.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReversedWords {
 
-    public static String reverseWords(String s){
-        return s;
-    }
+    public static String reverseWords(String str) {
 
+        final String[] allWords = str.split(" ");
+        Collections.reverse(Arrays.asList(allWords));
+
+        return String.join(" ", Arrays.asList(allWords));
+    }
 }
