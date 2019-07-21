f58758c4dc55e649fd43fe1759f08fa66ee096ae
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-21 23:12:37 +0200

    Highest scoring word Solved.  Refactoring.

--- highestScoringWord/src/main/java/Kata.java
+++ highestScoringWord/src/main/java/Kata.java
@@ -5,28 +5,15 @@ import java.util.Map;
 
 public class Kata {
 
-    private static int VALUE_LETTER_RANGE = 96;
-
     public static String high(String s) {
-
-        if (s.isEmpty()) {
-            return "";
-        }
 
         Map<String, Integer> mapOfResults = new HashMap<>();
 
         String[] allWords = s.split(" ");
         for (String word : allWords) {
-
-            final String[] letters = word.split("");
-            int result = 0;
-            for (String letter : letters) {
-                final char c = letter.charAt(0);
-                result += (c - VALUE_LETTER_RANGE);
-            }
-            mapOfResults.put(word, result);
+            final int resultOfWord = word.chars().map(c -> c - 'a').sum();
+            mapOfResults.put(word, resultOfWord);
         }
-
         return Collections.max(mapOfResults.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
     }
 }
