876d0fbcfd23fbec73f6a22715205c9ae21b8b6f
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-21 22:35:26 +0200

    Squashed commit of the following:
    commit 0638f9b3350d1ad61fa0b00523a9c15f1e1bb30a
    Merge: 046bf94 a624f57
    Author: Asystent Roberta <asystent.roberta@gmail.com>
    Date:   Sun Jul 21 22:33:16 2019 +0200
    
        Merge branch 'minorAdjustments'
    
        * minorAdjustments:
          Fix bug in "Number of people in the bus". During refactoring someone deleted code :).
    
    commit a624f57e11bfbe9009d8766266b154161bbac5ab
    Author: Asystent Roberta <asystent.roberta@gmail.com>
    Date:   Sun Jul 21 22:32:55 2019 +0200
    
        Fix bug in "Number of people in the bus". During refactoring someone deleted code :).
    
    commit 046bf94b18699c66627046beb572ba5d59cbc469
    Author: Asystent Roberta <asystent.roberta@gmail.com>
    Date:   Sun Jul 21 01:26:28 2019 +0200
    
        Highest scoring word - Solved. For the last time there was issue with empty string.
    
    commit 58766b3426c2e8a5ea6c4fea1b626b3e492e47da
    Author: Asystent Roberta <asystent.roberta@gmail.com>
    Date:   Sun Jul 21 00:50:00 2019 +0200
    
        First attempt to Highest Scoring Word. All test locally passed, but not the online tests.
    
    commit 4c9bb694ad32b000394bd75d9cdc9a60867a7943
    Author: Asystent Roberta <asystent.roberta@gmail.com>
    Date:   Sun Jul 21 00:24:45 2019 +0200
    
        Prepared to the next challenge. Highest scoring word.

---
+++ highestScoringWord/src/main/java/Kata.java
@@ -0,0 +1,32 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata {
+
+    private static int VALUE_LETTER_RANGE = 96;
+
+    public static String high(String s) {
+
+        if (s.isEmpty()) {
+            return "";
+        }
+
+        Map<String, Integer> mapOfResults = new HashMap<>();
+
+        String[] allWords = s.split(" ");
+        for (String word : allWords) {
+
+            final String[] letters = word.split("");
+            int result = 0;
+            for (String letter : letters) {
+                final char c = letter.charAt(0);
+                result += (c - VALUE_LETTER_RANGE);
+            }
+            mapOfResults.put(word, result);
+        }
+
+        return Collections.max(mapOfResults.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+    }
+}
---
+++ highestScoringWord/src/test/java/KataTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KataTest {
+
+    @Test
+    public void sampleTests() {
+
+        assertEquals("taxi", Kata.high("man i need a taxi up to ubud"));
+        assertEquals("volcano", Kata.high("what time are we climbing up to the volcano"));
+        assertEquals("semynak", Kata.high("take me to semynak"));
+    }
+
+    @Test
+    public void testWithEmptyString(){
+        assertEquals("", Kata.high(""));
+    }
+}
