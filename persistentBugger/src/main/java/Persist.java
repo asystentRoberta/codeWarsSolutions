0f90a285cd1dd97f0fdb4c76125afe8d33e62ec4
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-22 22:52:42 +0200

    Persistent bug. Refactor.

--- persistentBugger/src/main/java/Persist.java
+++ persistentBugger/src/main/java/Persist.java
@@ -2,12 +2,9 @@ public class Persist {
 
     public static int persistence(long n) {
 
-        if (n < 9) {
-            return 0;
-        }
-
         int resultToReturn = 0;
         String digitsOfNumber = String.valueOf(n);
+
         while (digitsOfNumber.length() != 1) {
 
             final long newNumber = digitsOfNumber.chars()
