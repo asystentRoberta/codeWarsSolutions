adf7716b2520fb7004c520d49a509c08412b7300
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-22 22:59:15 +0200

    Bring back the test from the codewars website.

--- persistentBugger/src/test/java/PersistTest.java
+++ persistentBugger/src/test/java/PersistTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 public class PersistTest {
 
-//    @Test
-//    public void basicTest() {
-//
-//        System.out.println("****** Basic Tests ******");
-//        assertEquals(3, Persist.persistence(39));
-//        assertEquals(0, Persist.persistence(4));
-//        assertEquals(2, Persist.persistence(25));
-//        assertEquals(4, Persist.persistence(999));
-//    }
+    @Test
+    public void basicTest() {
+
+        System.out.println("****** Basic Tests ******");
+        assertEquals(3, Persist.persistence(39));
+        assertEquals(0, Persist.persistence(4));
+        assertEquals(2, Persist.persistence(25));
+        assertEquals(4, Persist.persistence(999));
+    }
 
     @Test
     public void singleDigitTest() {
