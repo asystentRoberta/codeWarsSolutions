962a8cdcc9e4219a9bc3c3273d5ade9723866ad8
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-22 16:45:25 +0200

    Balanced number. Solutions from society.

--- balancedNumber/src/main/java/BalancedNumber.java
+++ balancedNumber/src/main/java/BalancedNumber.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
 
 public class BalancedNumber {
 
@@ -24,4 +25,50 @@ public class BalancedNumber {
             return "Not Balanced";
         }
     }
+
+    //Some long solution
+
+    private final String str;
+
+    private BalancedNumber(final long number) {
+
+        this.str = String.valueOf(number);
+    }
+
+    public static String balancedNumWithQuota(final long number) {
+
+        return new BalancedNumber(number).compute();
+    }
+
+    private String compute() {
+
+        return (isSmall() || leftSum() == rightSum()) ? "Balanced" : "Not Balanced";
+    }
+
+    private int rightSum() {
+
+        return str.chars().skip(str.length() / 2 + 1).map(toDigit).sum();
+    }
+
+    private int leftSum() {
+
+        return str.chars().limit((str.length() - 1) / 2).map(toDigit).sum();
+    }
+
+    private boolean isSmall() {
+
+        return str.length() < 3;
+    }
+
+    private static final IntUnaryOperator toDigit = c -> Character.digit(c, 10);
+
+    //End of long solution
+
+    public static String balancedNumCleverOne(long number){
+        String  str = String.valueOf(number);
+        int l = str.substring(0, str.length()/2 - str.length()%2 == 0 ? 1:0).chars().sum();
+        int r = str.substring(str.length()/2+1).chars().sum();
+        return (l==r ? "" : "Not ") + "Balanced";
+    }
 }
+
--- balancedNumber/src/test/java/BalancedNumberTest.java
+++ balancedNumber/src/test/java/BalancedNumberTest.java
@@ -23,4 +23,44 @@ public class BalancedNumberTest {
         assertEquals("Not Balanced", BalancedNumber.balancedNum(1230987));
         assertEquals("Balanced", BalancedNumber.balancedNum(56239814));
     }
+    
+    @Test
+    public void testBalancedNumWithQoutaForShortNumber(){
+        assertEquals("Balanced", BalancedNumber.balancedNumWithQuota(7));
+        assertEquals("Balanced", BalancedNumber.balancedNumWithQuota(959));
+        assertEquals("Balanced", BalancedNumber.balancedNumWithQuota(13));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumWithQuota(432));
+        assertEquals("Balanced", BalancedNumber.balancedNumWithQuota(424)); 
+    }
+    
+    @Test
+    public void testBalanedNumWithQuotaForLargerNumber(){
+
+        assertEquals("Not Balanced", BalancedNumber.balancedNumWithQuota(1024));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumWithQuota(66545));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumWithQuota(295591));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumWithQuota(1230987));
+        assertEquals("Balanced", BalancedNumber.balancedNumWithQuota(56239814));
+        
+    }
+
+    @Test
+    public void testBalancedNumCleverOneForShortNumber(){
+        assertEquals("Balanced", BalancedNumber.balancedNumCleverOne(7));
+        assertEquals("Balanced", BalancedNumber.balancedNumCleverOne(959));
+        assertEquals("Balanced", BalancedNumber.balancedNumCleverOne(13));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumCleverOne(432));
+        assertEquals("Balanced", BalancedNumber.balancedNumCleverOne(424));
+    }
+
+    @Test
+    public void testBalanedNumCleverOneForLargerNumber(){
+
+        assertEquals("Not Balanced", BalancedNumber.balancedNumCleverOne(1024));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumCleverOne(66545));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumCleverOne(295591));
+        assertEquals("Not Balanced", BalancedNumber.balancedNumCleverOne(1230987));
+        assertEquals("Balanced", BalancedNumber.balancedNumCleverOne(56239814));
+
+    }
 }
