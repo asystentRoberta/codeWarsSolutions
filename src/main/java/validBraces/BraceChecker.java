eaeac4d7d3ff63f7ff8003a7a927e345cc24d321
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-25 11:55:09 +0200

    First step. It is far away from solution :) Tests doesn't pass.

--- src/main/java/validBraces/BraceChecker.java
+++ src/main/java/validBraces/BraceChecker.java
@@ -2,8 +2,18 @@ package validBraces;
 
 public class BraceChecker {
 
-    public boolean isValid(String braces){
-        return false;
-    }
+    public boolean isValid(String braces) {
 
+
+
+        final long parentheses = braces.chars()
+                .filter(c -> c == ')' || c == '(')
+                .count();
+        final long brackets = braces.chars()
+                .filter(c -> c == '[' || c == ']')
+                .count();
+        final long curlyBraces = braces.length() - brackets - parentheses;
+
+        return (parentheses % 2 == 0 && brackets % 2 == 0 && curlyBraces % 2 == 0);
+    }
 }
