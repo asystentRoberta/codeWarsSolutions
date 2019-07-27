5a58ed0050981da719d6e084820d533b9988dbd1
Asystent Roberta <asystent.roberta@gmail.com>
2019-07-27 13:19:09 +0200

    Reverse Polish Notation Calculator solved (less more).

--- src/main/java/reversePolishNotationCalculator/Calc.java
+++ src/main/java/reversePolishNotationCalculator/Calc.java
@@ -1,9 +1,51 @@
 package reversePolishNotationCalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calc {
 
     public double evaluate(String expr) {
 
-        return 0;
+        if (expr.equals("")) {
+            return 0;
+        }
+        if(expr.matches("\\d*\\.\\d+") || expr.matches("\\d*")){
+            return Double.parseDouble(expr);
+        }
+
+        String[] arrayOfStringExpr = expr.split(" ");
+        List<Double> stackOfNumbers = new ArrayList<>();
+        int pointerOfTheStack = 0;
+        double resultOfExpresion = 0.0;
+
+        for (String s : arrayOfStringExpr) {
+            if (s.matches("\\d*\\.\\d+") || s.matches("\\d+")) {
+                stackOfNumbers.add(Double.valueOf(s));
+                pointerOfTheStack++;
+            } else {
+                switch (s) {
+                    case "+":
+                        resultOfExpresion =
+                                stackOfNumbers.get(pointerOfTheStack -2) + stackOfNumbers.get(pointerOfTheStack - 1);
+                        break;
+                    case "-":
+                        resultOfExpresion =
+                                stackOfNumbers.get(pointerOfTheStack -2) - stackOfNumbers.get(pointerOfTheStack - 1);
+                        break;
+                    case "*":
+                        resultOfExpresion =
+                                stackOfNumbers.get(pointerOfTheStack-2) * stackOfNumbers.get(pointerOfTheStack - 1);
+                        break;
+                    case "/":
+                        resultOfExpresion =
+                                stackOfNumbers.get(pointerOfTheStack-2) / stackOfNumbers.get(pointerOfTheStack - 1);
+                        break;
+                    default:
+                        System.out.println("Error");
+                }
+            }
+        }
+        return resultOfExpresion;
     }
 }
