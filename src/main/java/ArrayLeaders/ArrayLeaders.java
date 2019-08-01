8f3a3697c108e74ba51a460c7ef3045bd546d5cb
Asystent Roberta <asystent.roberta@gmail.com>
2019-08-01 16:53:51 +0200

    Solved. Little refactoring.

--- src/main/java/ArrayLeaders/ArrayLeaders.java
+++ src/main/java/ArrayLeaders/ArrayLeaders.java
@@ -10,29 +10,25 @@ public class ArrayLeaders {
 
     public static List arrayLeaders(int[] numbers) {
 
-        List<Integer> sumOfNextNumbers = new ArrayList<>();
         List<Integer> numbersList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
-
-        for (int i = 0; i < numbers.length; i++) {
-
-
-            int sum = numbersList.stream()
-                    .flatMapToInt(n->IntStream.of(n.intValue()))
-                    .sum();
-
-            sumOfNextNumbers.add(sum);
-            numbersList.remove(0);
-        }
-        sumOfNextNumbers.add(0);
         List<Integer> leadersOfNumbers = new ArrayList<>();
+        numbersList.remove(0);
 
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i]>sumOfNextNumbers.get(i+1)){
+        for (int i = 0; i < (numbers.length-1); i++) {
+            int sumOfNextNumbers = numbersList.stream()
+                    .flatMapToInt(n -> IntStream.of(n.intValue()))
+                    .sum();
+            if (sumOfNextNumbers < numbers[i]) {
                 leadersOfNumbers.add(numbers[i]);
             }
+            numbersList.remove(0);
+        }
 
+        if (numbers[numbers.length - 1]>0) {
+            leadersOfNumbers.add(numbers[numbers.length - 1]);
         }
 
         return leadersOfNumbers;
     }
 }
+
