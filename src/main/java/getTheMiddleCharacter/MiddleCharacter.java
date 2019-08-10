2831b49bde2d309e67a85ab9007eaaa5725b3f53
Asystent Roberta <asystent.roberta@gmail.com>
2019-08-10 15:17:09 +0200

    Refactoring

--- src/main/java/getTheMiddleCharacter/MiddleCharacter.java
+++ src/main/java/getTheMiddleCharacter/MiddleCharacter.java
@@ -3,8 +3,8 @@ package getTheMiddleCharacter;
 public class MiddleCharacter {
 
     public static String getMiddle(String word){
-        if(word.length()==1) return word;
-        if(word.length()%2==0) return word.substring(word.length()/2-1,word.length()/2+1);
+
+        if(word.length()%2==0) return word.substring(word.length()/2-1, word.length()/2+1);
         return word.substring(word.length()/2, word.length()/2+1);
     }
 
