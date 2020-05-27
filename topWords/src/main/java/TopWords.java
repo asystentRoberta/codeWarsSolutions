7b79a4860c5669b28bb907c87a7162a776e00c7e
AsystentRoberta <asystent.roberta@gmail.com>
2020-05-27 14:31:23 +0200

    Tests with TreeMap, HashMap and so on - all to bin

--- topWords/src/main/java/TopWords.java
+++ topWords/src/main/java/TopWords.java
@@ -1,12 +1,48 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TopWords {
 
     public static List<String> top3(String stringToTest) {
 
-        List<String> listOfWords = new ArrayList<>();
-        listOfWords.add(stringToTest);
-        return listOfWords;
+        List<String> wordToReturn = new ArrayList<>();
+        if (stringToTest.isEmpty()) {
+            return wordToReturn;
+        }
+        String cleanedString = stringToTest.replaceAll("[^\\x00-\\x7F]", "");
+        List<String> listOfWords = Arrays.asList(cleanedString.split(" "));
+        final List<String> collectedWords =
+                listOfWords.stream().filter(word -> !word.isEmpty()).collect(Collectors.toList());
+        int frequency = 0;
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        for (String word : collectedWords) {
+            frequency = Collections.frequency(listOfWords, word);
+            wordMap.put(word, frequency);
+        }
+
+        final Map<String, Integer> sortedWords = wordMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        sortedWords.entrySet().stream().forEach(System.out::println);
+
+        int i = 0;
+        for (Map.Entry<String, Integer> words : sortedWords.entrySet()) {
+
+            if (i < 3) {
+                wordToReturn.add(words.getKey());
+                i++;
+            }
+        }
+
+        return wordToReturn;
     }
 }
---
+++ topWords/src/test/java/TopWordsTest.java
@@ -0,0 +1,51 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.stream.*;
+import java.util.Arrays;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class TopWordsTest {
+
+    @Test
+    public void sampleTests() {
+        assertEquals(Arrays.asList("e", "d", "a"),    TopWords.top3("a a a  b  c c  d d d d  e e e e e"));
+        assertEquals(Arrays.asList("e", "ddd", "aa"), TopWords.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
+        assertEquals(Arrays.asList("won't", "wont"),  TopWords.top3("  //wont won't won't "));
+        assertEquals(Arrays.asList("e"),              TopWords.top3("  , e   .. "));
+        assertEquals(Arrays.asList(),                 TopWords.top3("  ...  "));
+        assertEquals(Arrays.asList(),                 TopWords.top3("  '  "));
+        assertEquals(Arrays.asList(),                 TopWords.top3("  '''  "));
+        assertEquals(Arrays.asList("a", "of", "on"),  TopWords.top3(Stream
+                .of("In a village of La Mancha, the name of which I have no desire to call to",
+                        "mind, there lived not long since one of those gentlemen that keep a lance",
+                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                        "coursing. An olla of rather more beef than mutton, a salad on most",
+                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                        "on Sundays, made away with three-quarters of his income.")
+                .collect(Collectors.joining("\n")) ));
+    }
+    @Test
+    public void longTextTest(){
+        assertEquals(Arrays.asList("a", "of", "on"),  TopWords.top3(Stream
+                .of("In a village of La Mancha, the name of which I have no desire to call to",
+                        "mind, there lived not long since one of those gentlemen that keep a lance",
+                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                        "coursing. An olla of rather more beef than mutton, a salad on most",
+                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                        "on Sundays, made away with three-quarters of his income.")
+                .collect(Collectors.joining("\n")) ));
+    }
+
+    @Test
+    public void toSeePrintOut(){
+        TopWords.top3("In a village of La Mancha, the name of which I have no desire to call to\",\n"
+                + "                        \"mind, there lived not long since one of those gentlemen that keep a lance\",\n"
+                + "                        \"in the lance-rack, an old buckler, a lean hack, and a greyhound for\",\n"
+                + "                        \"coursing. An olla of rather more beef than mutton, a salad on most\",\n"
+                + "                        \"nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra\",\n"
+                + "                        \"on Sundays, made away with three-quarters of his income.");
+    }
+
+}
